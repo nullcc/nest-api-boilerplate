52f19cd0df565703a644e463543bfff29d24db8c
Ethan Zhang <ethan.zhang@ringcentral.com>
2025-01-03 09:08:40 +0800

    chore(*) update data-source.ts

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,10 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { SeederOptions } from 'typeorm-extension';
 
-import { userFactory } from '@modules/user/testing/user.factory';
-import UserSeeder from '@modules/user/testing/user.seeder';
-import { User } from "@modules/user/entities/user.entity";
-
 export const dataSourceOptions: DataSourceOptions & SeederOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
@@ -15,8 +11,8 @@ export const dataSourceOptions: DataSourceOptions & SeederOptions = {
   synchronize: false,
   entities: [__dirname + '/../**/entities/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
-  factories: [userFactory],
-  seeds: [UserSeeder],
+  factories: [__dirname + '/../**/testing/*.factory{.ts,.js}'],
+  seeds: [__dirname + '/../**/testing/*.seeder{.ts,.js}'],
   extra: {
     ssl:
       process.env.SSL_MODE === 'require'
